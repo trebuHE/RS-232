e11c39c802b10b746aeb071a2a06fad1c40757d4
trebuHE <47220041+trebuHE@users.noreply.github.com>
2025-04-03 17:00:49 +0200

    Add rx, tx and adder to top module

--- rs_232.srcs/sources_1/new/rs232_transceiver.v
+++ rs_232.srcs/sources_1/new/rs232_transceiver.v
@@ -9,11 +9,31 @@ module rs232_transceiver(
     
     parameter BAUD_RATE = 9600;
     parameter CLK_FREQ = 100000000; // 100 MHz
-
-    reg [7:0] rx_data;
-    reg [7:0] modified_data;
-    reg [7:0] tx_data;
-    reg rx_ready;
-    reg tx_ready;
     
+    reg [7:0] rx_data;
+    reg rx_ready;
+    uart_rx rx (
+        .clk_i(clk_i),
+        .rst_i(rst_i),
+        .rx_i(RXD_i),
+        .data_o(rx_data),
+        .ready_o(rx_ready)
+        );
+
+    reg [7:0] modified_data;
+    add_0x20 adder (
+        .val_i(rx_data),
+        .val_o(modified_data)
+        );
+   
+    reg tx_ready;
+    uart_tx tx (
+        .clk_i(clk_i),
+        .rst_i(rst_i),
+        .data_i(modified_data),
+        .tx_o(TXD_o),
+        .ready_o(tx_ready)
+        );
+        
+        
 endmodule
